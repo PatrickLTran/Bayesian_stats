1afd3cb870e023898cf0ef1461a7e689d09a5f0e
PatrickLTran <40311782+PatrickLTran@users.noreply.github.com>
2023-04-28 01:05:58 -0700

    added estimation with pymc

--- linear_regression/prior_estimation.py
+++ linear_regression/prior_estimation.py
@@ -1,24 +1,35 @@
 #%%
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import numpy as np
-
+from scipy.stats import norm
+import pymc
 
 #%%
 
-df = pd.read_csv('/home/docker/stats_rethinking/linear_regression/Howell1.csv',sep=';')
+df = pd.read_csv('/home/docker/Bayesian_stats-main/linear_regression/Howell1.csv',sep=';')
 df_18 = df[df['age'] >= 18]
+h_bar = np.mean(df_18.height)
 
 #%%
 # ####
 """
 
-y = a + b(h_i - h_)
+mu = a + b(h_i - h_bar)
 
 """
-# %%
-h_bar = np.mean(df_18.height)
-
 #%%
+h = df_18.height.values
+with pymc.Model() as model:
+    a = pymc.Normal('intercept', mu=60,sigma=10)
+    b = pymc.Lognormal('slope', mu=0, sigma=1)
+    mu = a + b*(h-h_bar)
+    sigma = pymc.HalfNormal('Sigma', sigma=10)
+    y_obs = df_18.weight.values
 
-alpa = np.random.normal(loc=60,scale=10,size=5)
+with model:
+    trace = pymc.sample(1000,tune=1000)
+
+print(pymc.summary(trace, var_names=['intercept', 'slope']))
+
+# %%
